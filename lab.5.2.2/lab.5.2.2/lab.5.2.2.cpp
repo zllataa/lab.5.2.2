b82a2b715912bc27e9ea7c6780e9075016d43299
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-11-04 02:47:19 +0300

    Add files via upload

---
+++ lab.5.2.2/lab.5.2.2/lab.5.2.2.cpp
@@ -0,0 +1,64 @@
+﻿// Лабораторна робота No 5.2
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 1;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "ln(1-x)" << " |"
+		<< setw(7) << "-S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << log(1-x) << " |"
+			<< setw(10) << setprecision(5) << (-1)*s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+double S(const double x, const double eps, int& n, double s)
+{
+	n = 1; // вираз залежить від умови завдання варіанту
+	double a = x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+
+			a = A(x, n, a);
+		    s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
+double A(const double x, const int n, double a)
+{
+	double R = x * ((n-1) / n) ; // вираз залежить від умови завдання варіанту
+	a *= R;
+	return a;
+}
